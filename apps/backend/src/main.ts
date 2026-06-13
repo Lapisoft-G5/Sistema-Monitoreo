6896294108671efb5b927a6fde9ecb5ae90aba44
csalhuaa <csalhuaa@unsa.edu.pe>
2026-06-12 19:14:09 -0500

    chore(backend): bind app listen hostname to 0.0.0.0 for cloud environments

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -44,7 +44,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
   const logger = app.get(Logger);
   logger.log(`Backend running on ${await app.getUrl()}/api`);
 }

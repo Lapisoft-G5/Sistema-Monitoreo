a38447254af4c54e0931fb7ed7deb9b401ea0503
klismannSis <kchancuana@unsa.edu.pe>
2026-06-18 22:54:13 -0500

    feat(widgets): exportar EditarPlantillaForm en el index de plantillas

--- apps/frontend/src/widgets/plantillas/index.ts
+++ apps/frontend/src/widgets/plantillas/index.ts
@@ -1,3 +1,4 @@
 export * from './plantilla-form/ui/PlantillaForm';
 export * from './plantilla-form/ui/PlantillaCabecera';
 export * from './plantilla-form/ui/PlantillaDesempenos';
+export * from './plantilla-form/ui/EditarPlantillaForm';
